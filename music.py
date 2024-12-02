25c4723cd7b764c2d27732a194285b92fec4f642
Annabelladududu <duy@ufl.edu>
2024-12-02 13:28:28 -0500

    Update music.py
    add comments for music.py

--- music.py
+++ music.py
@@ -2,10 +2,10 @@ import pygame.mixer
 
 # set up the BGM
 def music():
-    pygame.mixer.init()
+    pygame.mixer.init() #initialize the audio system to play music
     try:
         pygame.mixer.music.load("8-bit-retro-game-music-233964.mp3")
-        pygame.mixer.music.set_volume(0.5)
-        pygame.mixer.music.play(-1)
-    except pygame.error as e:
+        pygame.mixer.music.set_volume(0.5) #set volume
+        pygame.mixer.music.play(-1) #music will loop infinitely if not stopped manually
+    except pygame.error as e: #handle pygame error
         print(f"Cannot load BGM...：{e}")
