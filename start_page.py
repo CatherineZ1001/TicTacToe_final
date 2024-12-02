03e714b206bd49b5a708f446a7d143abef85728e
Annabelladududu <duy@ufl.edu>
2024-12-02 12:20:10 -0500

    Update start_page.py
    add detailed comments to start page

--- start_page.py
+++ start_page.py
@@ -23,13 +23,17 @@ def start_page(screen):
         pygame.draw.rect(screen, ORANGE, settings_button)
 
         #put 'Start Game' to centre of the start button
+        #calculate x and y for the start button
         x_start_button = start_button.x + (start_button.width - start_text.get_width()) // 2
         y_start_button = start_button.y + (start_button.height - start_text.get_height()) // 2
+        #place start button
         screen.blit(start_text, (x_start_button, y_start_button))
 
         #put the 'Settings' to the centre of the settings button
+        #calcuate x and y for the settings button
         x_settings_button = settings_button.x + (settings_button.width - settings_text.get_width()) // 2
         y_settings_button = settings_button.y + (settings_button.height - settings_text.get_height()) // 2
+        #place settings button
         screen.blit(settings_text, (x_settings_button, y_settings_button))
 
         #event
@@ -38,9 +42,9 @@ def start_page(screen):
                pygame.quit()
                sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if start_button.collidepoint(event.pos):#click 'Start Game' button to start game.py
+                if start_button.collidepoint(event.pos): #click the start button to start game
                     return "START_GAME"
-                elif settings_button.collidepoint(event.pos):#click 'Settings' button directing to settings
+                elif settings_button.collidepoint(event.pos):#click settings button to open settings
                     return "SETTINGS"
 
         pygame.display.update()
