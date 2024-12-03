0836ac68b156d63109a4e771cb00c946049f8249
Annabelladududu <duy@ufl.edu>
2024-12-02 22:35:37 -0500

    Update start_page.py

--- start_page.py
+++ start_page.py
@@ -24,17 +24,17 @@ def start_page(screen):
 
         #put 'Start Game' to centre of the start button
         #calculate x and y for the start button
-        x_start_button = start_button.x + (start_button.width - start_text.get_width()) // 2
-        y_start_button = start_button.y + (start_button.height - start_text.get_height()) // 2
+        x_start_text = start_button.x + (start_button.width - start_text.get_width()) // 2
+        y_start_text = start_button.y + (start_button.height - start_text.get_height()) // 2
         #place start button
-        screen.blit(start_text, (x_start_button, y_start_button))
+        screen.blit(start_text, (x_start_text, y_start_text))
 
         #put the 'Settings' to the centre of the settings button
         #calcuate x and y for the settings button
-        x_settings_button = settings_button.x + (settings_button.width - settings_text.get_width()) // 2
-        y_settings_button = settings_button.y + (settings_button.height - settings_text.get_height()) // 2
+        x_settings_text = settings_button.x + (settings_button.width - settings_text.get_width()) // 2
+        y_settings_text = settings_button.y + (settings_button.height - settings_text.get_height()) // 2
         #place settings button
-        screen.blit(settings_text, (x_settings_button, y_settings_button))
+        screen.blit(settings_text, (x_settings_text, y_settings_text))
 
         #event
         for event in pygame.event.get():
@@ -42,7 +42,7 @@ def start_page(screen):
                pygame.quit()
                sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if start_button.collidepoint(event.pos): #click the start button to start game
+                if start_button.collidepoint(event.pos): #clic the start button to start game
                     return "START_GAME"
                 elif settings_button.collidepoint(event.pos):#click settings button to open settings
                     return "SETTINGS"
